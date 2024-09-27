cc090e75dcbbe2540280e468b78017209d1f512e
yoonju3221 <jyj_3221@naver.com>
2024-09-27 09:17:56 +0900

    feat : 경로 추가

--- kcalfit/routes.py
+++ kcalfit/routes.py
@@ -14,3 +14,28 @@ def setup_routes(app):
         result = get_data_from_db()
         print(result)
         return jsonify(result)  # result는 JSON으로 변환 가능한 딕셔너리
+    
+    @app.route('/diet')
+    def diet():
+        with open('templates/diet.html', 'r', encoding='utf-8') as file:
+            return file.read()
+        
+    @app.route('/moisture')
+    def moisture():
+        with open('templates/moisture.html', 'r', encoding='utf-8') as file:
+            return file.read()
+
+    @app.route('/exercise')
+    def exercise():
+        with open('templates/exercise.html', 'r', encoding='utf-8') as file:
+            return file.read()
+
+    @app.route('/alarm')
+    def alarm():
+        with open('templates/alarm.html', 'r', encoding='utf-8') as file:
+            return file.read()
+
+    @app.route('/mypage')
+    def mypage():
+        with open('templates/mypage.html', 'r', encoding='utf-8') as file:
+            return file.read()                
